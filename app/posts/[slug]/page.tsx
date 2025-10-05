c9a1ab014691e2b45196fcc1bd61530262ef3a7a
skufig <31157024+skufig@users.noreply.github.com>
2025-10-06 02:24:31 +0300

    Add files via upload

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -1,1 +1,6 @@
-
+import fs from 'fs'
+import path from 'path'
+import matter from 'gray-matter'
+import { remark } from 'remark'
+import html from 'remark-html'
+export default async function Post({ params }: any){ const slug = params.slug; const p = path.join(process.cwd(),'content','posts', `${slug}.md`); const raw = fs.readFileSync(p,'utf8'); const { data, content } = matter(raw); const processed = await remark().use(html).process(content); const body = String(processed); return (<section className="container py-12"><h1 className="text-3xl font-bold text-gold">{data.title}</h1><div className="mt-4 text-muted" dangerouslySetInnerHTML={{__html: body}} /></section>)}
