ac229cd3e7adbbdf09ffad1b351690d3da7eb337
skufig <31157024+skufig@users.noreply.github.com>
2025-10-06 03:12:08 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,16 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        graphite: "#0b0b0b",
+        gold: "#d4af37",
+        muted: "#9b9b9b",
+      },
+    },
   },
   plugins: [],
 };
